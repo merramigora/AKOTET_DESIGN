48a615d2d17cbcc2815f9d9c19ba866b9c72487a
merramigora <132627180+merramigora@users.noreply.github.com>
2024-10-09 00:55:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.order-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Thank you for your order from AKOTET DESIGN!');
+    });
+});
